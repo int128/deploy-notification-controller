5f813722e9aed6a097e16fd0965c5f97acb7893b
Hidetake Iwata <int128@gmail.com>
2021-06-30 16:24:12 +0900

    Watch status of replica sets

--- controllers/monitor_controller.go
+++ controllers/monitor_controller.go
@@ -18,13 +18,15 @@ package controllers
 
 import (
 	"context"
+	"time"
 
+	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	deploynotificationv1 "github.com/int128/deploy-notification-controller/api/v1"
 )
 
 // MonitorReconciler reconciles a Monitor object
@@ -47,9 +49,24 @@ type MonitorReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *MonitorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// your logic here
+	var rs appsv1.ReplicaSet
+	if err := r.Get(ctx, req.NamespacedName, &rs); err != nil {
+		logger.Error(err, "unable to fetch ReplicaSet")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	logger.Info("changed status to", "status", rs.Status, "conditions", rs.Status.Conditions)
+
+	if rs.Status.ReadyReplicas == rs.Status.Replicas {
+		logger.Info("ready", "time_to_ready", time.Now().Sub(rs.CreationTimestamp.Time))
+		return ctrl.Result{}, nil
+	}
+	if rs.Status.FullyLabeledReplicas == rs.Status.Replicas {
+		logger.Info("not ready", "time_to_complete", time.Now().Sub(rs.CreationTimestamp.Time))
+		return ctrl.Result{}, nil
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -57,6 +74,39 @@ func (r *MonitorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 // SetupWithManager sets up the controller with the Manager.
 func (r *MonitorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&deploynotificationv1.Monitor{}).
+		For(&appsv1.ReplicaSet{}, builder.WithPredicates(&StatusChangePredicate{})).
 		Complete(r)
+}
+
+type StatusChangePredicate struct{}
+
+func (l StatusChangePredicate) Create(_ event.CreateEvent) bool {
+	return false
+}
+
+func (l StatusChangePredicate) Delete(_ event.DeleteEvent) bool {
+	return false
+}
+
+func (l StatusChangePredicate) Update(e event.UpdateEvent) bool {
+	oldReplicaSet, ok := e.ObjectOld.(*appsv1.ReplicaSet)
+	if !ok {
+		return false
+	}
+	newReplicaSet, ok := e.ObjectNew.(*appsv1.ReplicaSet)
+	if !ok {
+		return false
+	}
+
+	if oldReplicaSet.Status.ReadyReplicas != newReplicaSet.Status.ReadyReplicas {
+		return true
+	}
+	if oldReplicaSet.Status.FullyLabeledReplicas != newReplicaSet.Status.FullyLabeledReplicas {
+		return true
+	}
+	return false
+}
+
+func (l StatusChangePredicate) Generic(_ event.GenericEvent) bool {
+	return false
 }
